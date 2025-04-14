1d4740674c304e409ad85297c2dc05877a96f519
Matthew Feickert <matthew.feickert@cern.ch>
2025-04-13 18:25:53 -0600

    Update to Ubuntu noble numbat images

--- examples/hello_pytorch/build.sh
+++ examples/hello_pytorch/build.sh
@@ -3,13 +3,13 @@
 docker system prune -f
 
 docker pull tonistiigi/binfmt
-docker pull ghcr.io/prefix-dev/pixi:jammy
-docker pull ghcr.io/prefix-dev/pixi:jammy-cuda-12.6.3
+docker pull ghcr.io/prefix-dev/pixi:noble
+docker pull ghcr.io/prefix-dev/pixi:noble-cuda-12.6.3
 
 docker build \
     --file Dockerfile \
     --platform linux/amd64 \
-    --tag uw-madison-dsi/hello_pytorch:jammy-cuda-12.6.3 \
+    --tag uw-madison-dsi/hello_pytorch:noble-cuda-12.6.3 \
     .
 
 docker images uw-madison-dsi/hello_pytorch
