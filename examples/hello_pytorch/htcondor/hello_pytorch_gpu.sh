eef04170dac7ff25fbf29d060bd7179fa6e3fd67
Matthew Feickert <matthew.feickert@cern.ch>
2025-05-02 10:50:40 -0600

    Source the Pixi environment in the execute script

--- examples/hello_pytorch/htcondor/hello_pytorch_gpu.sh
+++ examples/hello_pytorch/htcondor/hello_pytorch_gpu.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 echo -e "# Hello CHTC from Job ${1} running on $(hostname)\n"
 
+echo -e "# Activating Pixi environment\n"
+. /app/entrypoint.sh
+
 echo -e "# Check to see if the NVIDIA drivers can correctly detect the GPU:\n"
 nvidia-smi
 
 echo -e "# Check if PyTorch can detect the GPU:\n"
-python ./src/torch_detect_GPU.py
+python /app/src/torch_detect_GPU.py
