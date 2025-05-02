86ea6628e833e1d442524f73d66e141d1bf520b4
Matthew Feickert <matthew.feickert@cern.ch>
2025-05-02 00:18:13 -0600

    feat: Add HTCondor basic submission tests

---
+++ examples/hello_pytorch/htcondor/README.md
@@ -0,0 +1,6 @@
+# Running the Hello PyTorch example on CHTC
+
+This example contains two files:
+
+* `hello_pytorch.sub`: The HTCondor job submission file
+* `hello_pytorch.sh`: The commands that are run as the job
---
+++ examples/hello_pytorch/htcondor/hello_pytorch.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo -e "# Hello CHTC from Job ${1} running on $(hostname)\n"
+
+
+echo -e "# Check to see if the NVIDIA drivers can correctly detect the GPU:\n"
+nvidia-smi
+
+echo -e "# Check if PyTorch can detect the GPU:\n"
+python ./src/torch_detect_GPU.py
