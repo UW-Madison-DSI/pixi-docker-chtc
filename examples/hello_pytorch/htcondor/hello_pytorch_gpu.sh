0ec6635c3ffd69398d05c2c5918cfacd61ada9a7
Matthew Feickert <matthew.feickert@cern.ch>
2025-05-02 11:57:44 -0600

    Guard against exec with 0 as argument

--- examples/hello_pytorch/htcondor/hello_pytorch_gpu.sh
+++ examples/hello_pytorch/htcondor/hello_pytorch_gpu.sh
@@ -2,7 +2,11 @@
 echo -e "# Hello CHTC from Job ${1} running on $(hostname)\n"
 
 echo -e "# Activating Pixi environment\n"
-. /app/entrypoint.sh
+# The last line of the entrypoint.sh file is 'exec "$@"'. If this shell script
+# receives arguments, exec will interpret them as arguments to it, which is not
+# intended. To avoid this, strip the last line of entrypoint.sh and source that
+# instead.
+. <(sed '$d' /app/entrypoint.sh)
 
 echo -e "# Check to see if the NVIDIA drivers can correctly detect the GPU:\n"
 nvidia-smi
