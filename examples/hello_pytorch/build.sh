f6b86c733b16ee0359fc3e3d2adc9080d2e14a3d
Matthew Feickert <matthew.feickert@cern.ch>
2025-05-01 23:26:39 -0600

    ci: Add publishing workflow of container image (#2)
    * Add GitHub Actions based Linux container build and publication workflow.
    * Update image names and tags to reflect use of GitHub Container Registry (ghcr.io).

--- examples/hello_pytorch/build.sh
+++ examples/hello_pytorch/build.sh
@@ -9,7 +9,7 @@ docker pull ghcr.io/prefix-dev/pixi:noble-cuda-12.6.3
 docker build \
     --file Dockerfile \
     --platform linux/amd64 \
-    --tag uw-madison-dsi/hello_pytorch:noble-cuda-12.6.3 \
+    --tag ghcr.io/uw-madison-dsi/pixi-docker-chtc:hello-pytorch-noble-cuda-12.6.3 \
     .
 
-docker images uw-madison-dsi/hello_pytorch
+docker images ghcr.io/uw-madison-dsi/pixi-docker-chtc
