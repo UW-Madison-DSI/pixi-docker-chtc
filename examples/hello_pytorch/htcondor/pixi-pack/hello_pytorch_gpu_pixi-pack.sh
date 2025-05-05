eced8b88801ced0a772315de4be673bceb7bec60
Matthew Feickert <matthew.feickert@cern.ch>
2025-05-05 12:04:26 -0600

    Update submission files

--- examples/hello_pytorch/htcondor/pixi-pack/hello_pytorch_gpu_pixi-pack.sh
+++ examples/hello_pytorch/htcondor/pixi-pack/hello_pytorch_gpu_pixi-pack.sh
@@ -6,18 +6,17 @@ set -x
 echo -e "# Hello CHTC from Job ${1} running on $(hostname)\n"
 echo -e "# GPUs assigned: ${CUDA_VISIBLE_DEVICES}\n"
 
-echo -e "# Activate Pixi environment\n"
-# The last line of the entrypoint.sh file is 'exec "$@"'. If this shell script
-# receives arguments, exec will interpret them as arguments to it, which is not
-# intended. To avoid this, strip the last line of entrypoint.sh and source that
-# instead.
-. <(sed '$d' /app/entrypoint.sh)
+echo -e "# Unpack and install pixi-pack environment\n"
+./environment.sh
+
+echo -e "# Activate environment\n"
+. activate.sh
 
 echo -e "# Check to see if the NVIDIA drivers can correctly detect the GPU:\n"
 nvidia-smi
 
 echo -e "\n# Check if PyTorch can detect the GPU:\n"
-python /app/src/torch_detect_GPU.py
+python torch_detect_GPU.py
 
 echo -e "\n# Extract the training data:\n"
 if [ -f "MNIST_data.tar.gz" ]; then
@@ -31,4 +30,4 @@ fi
 echo -e "\n# Train MNIST with PyTorch:\n"
 mkdir -p run
 cd run
-time python /app/src/torch_MNIST.py --epochs 14 --save-model
+time python ../torch_MNIST.py --epochs 14 --save-model
