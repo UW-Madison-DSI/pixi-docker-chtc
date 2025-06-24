b3270bafbe25c42b37cc1c5a7552a6ea4dfa759d
Matthew Feickert <matthew.feickert@cern.ch>
2025-06-23 18:09:14 -0600

    chore: Use commit SHA to tag Apptainer container SIF file
    * Use the commit SHA to tag the Apptainer container .sif file during build
      to have a more consistent tag system.

--- examples/hello_pytorch/build_apptainer.sh
+++ examples/hello_pytorch/build_apptainer.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-apptainer build pixi-docker-chtc.sif apptainer.def
+_sha=$(git rev-parse --verify --short HEAD)
+apptainer build pixi-docker-chtc-sha-"${_sha}".sif apptainer.def
 
-# apptainer run --nv --containall pixi-docker-chtc.sif bash
+# apptainer run --nv --containall pixi-docker-chtc*.sif bash
 
 # Note: The %runscript in the definition file is used to pass commands into the environment
-# apptainer run --nv --containall pixi-docker-chtc.sif python /app/src/torch_detect_GPU.py
+# apptainer run --nv --containall pixi-docker-chtc*.sif python /app/src/torch_detect_GPU.py
 
 # Note: 'apptainer exec' ignores the %runscript and the %startscript from the definition file
-# apptainer exec --nv --containall pixi-docker-chtc.sif /app/entrypoint.sh python /app/src/torch_detect_GPU.py
+# apptainer exec --nv --containall pixi-docker-chtc*.sif /app/entrypoint.sh python /app/src/torch_detect_GPU.py
