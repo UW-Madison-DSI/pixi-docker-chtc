ac26cdcbb39169af102a684f2afbc33de586acd9
Matthew Feickert <matthew.feickert@cern.ch>
2025-08-31 01:29:20 +0200

    feat: Add SLURM example

---
+++ examples/hello_pytorch/slurm/hello_pytorch_gpu_slurm.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#SBATCH --job-name="a.out_symmetric"
+#SBATCH --output="a.out.%j.%N.out"
+#SBATCH --partition=gpuA40x4
+#SBATCH --mem=10G
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=8
+#SBATCH --constraint="scratch"
+#SBATCH --gpus-per-node=1
+#SBATCH --gpu-bind=closest  # select a cpu close to gpu on pci bus topology
+#SBATCH --account=bexo-delta-gpu  # match to a "Project" returned by the "accounts" command
+#SBATCH --exclusive  # dedicated node for this job
+#SBATCH --no-requeue
+#SBATCH -t 04:00:00
+#SBATCH -e slurm-%j.err
+#SBATCH -o slurm-%j.out
+
+module reset
+
+echo "job is starting on $(hostname)"
+
+nvidia-smi
+
+srun execute.sh
