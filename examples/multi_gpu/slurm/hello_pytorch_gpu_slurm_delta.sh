1268e917b1dedde4ebd2ad7cab901261d3a2bded
Matthew Feickert <matthew.feickert@cern.ch>
2026-01-05 23:51:49 -0700

    Add skeleton of multi-GPU workflow

---
+++ examples/multi_gpu/slurm/hello_pytorch_gpu_slurm_delta.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+#SBATCH --job-name="hello_pytorch"
+#SBATCH --partition=gpuA40x4
+#SBATCH --mem=2G
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=1
+#SBATCH --constraint="scratch"
+#SBATCH --gpus-per-node=1
+#SBATCH --gpu-bind=closest  # select a cpu close to gpu on pci bus topology
+#SBATCH --account=<ACCOUNT NAME>  # match to an "Account" returned by the 'accounts' command
+#SBATCH --exclusive  # dedicated node for this job
+#SBATCH --no-requeue
+#SBATCH --time=01:00:00
+#SBATCH --error hello_pytorch.slurm-%j.err
+#SBATCH --output hello_pytorch.slurm-%j.out
+
+module reset
+
+echo "job is starting on $(hostname)"
+
+nvidia-smi
+
+srun execute.sh
