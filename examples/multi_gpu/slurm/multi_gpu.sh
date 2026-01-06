131aec1de152842b932af1b18c4d42fe97a6acca
Matthew Feickert <matthew.feickert@cern.ch>
2026-01-06 00:21:01 -0700

    update example
    * c.f. https://docs.ncsa.illinois.edu/systems/delta/en/latest/user_guide/running_jobs.html#gpus-together-on-a-compute-node

--- examples/multi_gpu/slurm/multi_gpu.sh
+++ examples/multi_gpu/slurm/multi_gpu.sh
@@ -2,9 +2,9 @@
 
 #SBATCH --job-name="multi_gpu"
 #SBATCH --partition=gpuA40x4
-#SBATCH --mem=2G
+#SBATCH --mem=4G
 #SBATCH --nodes=1
-#SBATCH --ntasks-per-node=2
+#SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=4
 #SBATCH --constraint="scratch"
 #SBATCH --gpus-per-node=2
